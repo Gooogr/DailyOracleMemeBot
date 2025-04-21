daa9e65857028ee503e4e63f7f33f9f70a64f030
Grigoriy <gusarovga@ya.ru>
2025-04-21 21:34:37 +0300

    feat: minimal code for minio client and tg bot

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import os
+import random
+from io import BytesIO
 
 import telebot
 from dotenv import load_dotenv
+from minio import Minio
 
 load_dotenv()
+
+BUCKET_NAME = "test"
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
@@ -11,13 +16,44 @@ if not BOT_TOKEN:
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+minio_client = Minio(
+    "minio:9000",
+    access_key=os.getenv("MINIO_ROOT_USER"),
+    secret_key=os.getenv("MINIO_ROOT_PASSWORD"),
+    secure=False,
+)
+
+
+def get_random_image():
+    objects = list(minio_client.list_objects(BUCKET_NAME, recursive=True))
+    if not objects:
+        return None
+
+    obj = random.choice(objects)
+    response = minio_client.get_object(BUCKET_NAME, obj.object_name)
+
+    image_data = BytesIO(response.read())
+    image_data.name = obj.object_name  # Telegram API uses this as filename
+    return image_data
+
 
 @bot.message_handler(
-    func=lambda message: True,
+    commands=["echo"],
     content_types=["text"],
 )
 def echo_message(message):
     bot.reply_to(message, message.text)
+
+
+@bot.message_handler(
+    commands=["ask_oracle"],
+)
+def get_meme_from_minio(message):
+    image = get_random_image()
+    if image:
+        bot.send_photo(message.chat.id, photo=image)
+    else:
+        bot.reply_to(message, "can't show image")
 
 
 if __name__ == "__main__":
