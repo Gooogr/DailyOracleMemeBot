1183cde9707d92e52d2d466930034db81d44c1ac
Grigoriy <gusarovga@ya.ru>
2025-04-27 13:16:18 +0300

    feat: add alchemy models

---
+++ app/db/models.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, ForeignKey, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.sql import func
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Items(Base):
+    __tablename__ = "items"
+    id: Mapped[str] = mapped_column(String())
+    type: Mapped[str] = mapped_column(String(10))
+    upload_dt: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+
+    def __repr__(self) -> str:
+        return f"Item(id={self.id!r}, type={self.type!r}, upload_dt={self.upload_dt!r})"
+
+
+class Interactions(Base):
+    __tablename__ = "interactions"
+    user_id: Mapped[str] = mapped_column(String())
+    item_id: Mapped[str] = mapped_column(ForeignKey("items.id"))
+    interaction_dt: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+
+    def __repr__(self) -> str:
+        return f"Interaction(user_id={self.user_id!r}, item_id={self.item_id!r}, interaction_dt={self.interaction_dt!r})"
