dd46b331ae3e6d6569c421c794dcd8c3b00c0cd0
Grigoriy <gusarovga@ya.ru>
2025-05-04 01:07:20 +0300

    feat: expand service methods

--- app/service/service.py
+++ app/service/service.py
@@ -4,7 +4,12 @@ class MemeOracleService:
         self.db = db
 
     def get_db_item_id(self, user_id: str):
+        # implement logic of item selection based on interaction history/time/etc in db
         pass
+
+    def get_object(self, item_id: str):
+        return self.storage.get_object(object_id=item_id)
 
     def get_random_object(self):
         return self.storage.get_random_object()
+
