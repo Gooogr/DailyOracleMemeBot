fa1c3029f79b5671eb91fd570b641b6ac49f1bbf
ksyuwish <ksenija.vishnevskaya@ya.ru>
2025-05-09 01:39:22 +0300

    feat: add argparse

--- scripts/tg_parser/tg_parser.py
+++ scripts/tg_parser/tg_parser.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 import os
 from datetime import datetime
@@ -12,16 +13,14 @@ api_hash = os.getenv("API_HASH")
 channel_id = os.getenv("CHANNEL_ID")
 invite_link = os.getenv("INVITE_LINK")
 
-# Указываем даты в формате YYYY-MM-DD
-START_DATE = datetime.strptime("2025-05-03", "%Y-%m-%d")
-END_DATE = datetime.strptime("2025-05-07", "%Y-%m-%d")
-
 session_name = "my_session"
 save_path = os.path.expanduser("~/Downloads/meme_media2")
 os.makedirs(save_path, exist_ok=True)
 
 
-async def main():
+async def main(start_date: str, end_date: str):
+    start_date = datetime.strptime(start_date, "%Y-%m-%d")
+    end_date = datetime.strptime(end_date, "%Y-%m-%d")
     async with Client(session_name, api_id=api_id, api_hash=api_hash) as app:
         print("📥 Client started. Ensuring access to the channel...")
 
@@ -34,15 +33,17 @@ async def main():
 
         count = 0
         async for msg in app.get_chat_history(channel_id):
-            if msg.date < START_DATE:
+            if msg.date < start_date:
                 break  # История идёт в обратном порядке, можно остановиться
-            if msg.date > END_DATE:
+            if msg.date > end_date:
                 continue
 
             if msg.photo or msg.video:
                 count += 1
                 date_str = msg.date.strftime("%Y-%m-%d_%H-%M-%S")
-                ext = ".jpg" if msg.photo else ".mp4"
+                ext = (
+                    ".jpg" if msg.photo else ".mp4"
+                )  # TODO: check object extension in tg channel.
                 filename = f"{date_str}{ext}"
                 filepath = os.path.join(save_path, filename)
 
@@ -56,4 +57,16 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    parser = argparse.ArgumentParser(
+        prog="Tg parser",
+        description="Parse images and videos from private tg channel",
+    )
+    parser.add_argument(
+        "-s", "--start-date", type=str, help="Start date in format YYYY-MM-DD"
+    )
+    parser.add_argument(
+        "-e", "--end-date", type=str, help="End date in format YYYY-MM-DD"
+    )
+    args = parser.parse_args()
+    print(f"Selected date range [{args.start_date} ... {args.end_date}]")
+    asyncio.run(main(args.start_date, args.end_date))
