d7233b31bc4293f94ad7f0d74081d2866e7baa04
ksyuwish <ksenija.vishnevskaya@ya.ru>
2025-05-10 22:36:45 +0300

    feat: update script

--- scripts/tg_parser/parser.py
+++ scripts/tg_parser/parser.py
@@ -1,41 +1,36 @@
 import argparse
 import asyncio
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
-from dotenv import load_dotenv
 from pyrogram import Client
-
-load_dotenv("scripts/tg_parser/.env")
-
-api_id = int(os.getenv("API_ID"))
-api_hash = os.getenv("API_HASH")
-channel_id = os.getenv("CHANNEL_ID")
-invite_link = os.getenv("INVITE_LINK")
-
-session_name = "my_session"
-save_path = os.path.expanduser("~/Downloads/meme_media2")
-os.makedirs(save_path, exist_ok=True)
+from config import Config, load_config
 
 
-async def main(start_date: str, end_date: str):
-    start_date = datetime.strptime(start_date, "%Y-%m-%d")
-    end_date = datetime.strptime(end_date, "%Y-%m-%d")
-    async with Client(session_name, api_id=api_id, api_hash=api_hash) as app:
-        print("📥 Client started. Ensuring access to the channel...")
+async def main(
+    cfg: Config,
+    start_date: datetime,
+    end_date: datetime,
+    save_dir: str,
+    session_name="my_session"
+) -> None:
+    os.makedirs(save_dir, exist_ok=True)
 
+    print(f"Selected date range [{start_date} ... {end_date}]")
+
+    async with Client(session_name, api_id=cfg.api_id, api_hash=cfg.api_hash) as app:
         try:
-            chat = await app.get_chat(invite_link)  # resolve from invite
+            chat = await app.get_chat(cfg.invite_link)  # resolve from invite
             print(f"✅ Resolved channel: {chat.title} (id: {chat.id})")
         except Exception as e:
             print(f"❌ Could not resolve chat from invite: {e}")
             return
 
         count = 0
-        async for msg in app.get_chat_history(channel_id):
+        async for msg in app.get_chat_history(cfg.channel_id):
             if msg.date < start_date:
                 break  # История идёт в обратном порядке, можно остановиться
-            if msg.date > end_date:
+            if msg.date > end_date + timedelta(days=1):
                 continue
 
             if msg.photo or msg.video:
@@ -43,9 +38,9 @@ async def main(start_date: str, end_date: str):
                 date_str = msg.date.strftime("%Y-%m-%d_%H-%M-%S")
                 ext = (
                     ".jpg" if msg.photo else ".mp4"
-                )  # TODO: check object extension in tg channel.
+                )
                 filename = f"{date_str}{ext}"
-                filepath = os.path.join(save_path, filename)
+                filepath = os.path.join(save_dir, filename)
 
                 result = await app.download_media(msg, file_name=filepath)
                 if result and os.path.exists(result):
@@ -57,6 +52,7 @@ async def main(start_date: str, end_date: str):
 
 
 if __name__ == "__main__":
+    cfg = load_config()
     parser = argparse.ArgumentParser(
         prog="Tg parser",
         description="Parse images and videos from private tg channel",
@@ -67,6 +63,16 @@ if __name__ == "__main__":
     parser.add_argument(
         "-e", "--end-date", type=str, help="End date in format YYYY-MM-DD"
     )
+    parser.add_argument(
+        "-d", "--save-dir", type=str, default="./data", help="Saving dir for parsed objects"
+    )
     args = parser.parse_args()
-    print(f"Selected date range [{args.start_date} ... {args.end_date}]")
-    asyncio.run(main(args.start_date, args.end_date))
+    
+    asyncio.run(
+        main(
+            cfg=cfg,
+            start_date=datetime.strptime(args.start_date, "%Y-%m-%d"),
+            end_date=datetime.strptime(args.end_date, "%Y-%m-%d"),
+            save_dir=args.save_dir,
+        )
+    )
