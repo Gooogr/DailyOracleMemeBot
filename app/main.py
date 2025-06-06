9153c1316799236a3aba885e72c412c8b34435d1
Grigoriy <gusarovga@ya.ru>
2025-06-06 18:25:16 +0300

    fix: update application layer structure

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from minio import Minio
+import telebot
 
 from app.bot.bot import MemeOracleBot
 from app.database.postgres.factory import (
@@ -18,6 +19,7 @@ load_dotenv(dotenv_path="../.env")
 
 
 def main():
+    # Set up storage layer
     minio_client = Minio(
         endpoint="minio:9000",
         access_key=os.getenv("MINIO_ROOT_USER"),
@@ -28,6 +30,7 @@ def main():
         minio_client,
         os.getenv("MINIO_BUCKET_NAME"),
     )
+    # Set up database layer
     provider = PostgresProvider(
         user=os.getenv("POSTGRES_USER"),
         password=os.getenv("POSTGRES_PASSWORD"),
@@ -36,12 +39,16 @@ def main():
     item_repo_factory = PostgresItemRepositoryFactory()
     interaction_repo_factory = PostgresInteractionRepositoryFactory()
 
+    # Set up service layer
     item_service = ItemService(provider, item_repo_factory)
     interaction_service = InteractionService(provider, interaction_repo_factory)
     service = MemeOracleService(item_service, interaction_service, storage)
 
-    bot = MemeOracleBot(service, os.getenv("BOT_TOKEN"))
-    bot.run()
+    # Set up bot
+    bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
+    oracle = MemeOracleBot(service, bot)
+
+    oracle.run()
 
 
 if __name__ == "__main__":
